032bfb7ea916a80f960be85d2b19c4083ec94e3c
Taegyeong Lee <hanarotg@naver.com>
2022-11-16 00:32:43 +0900

    [add] 연결 상태 변수 추가 및 background 조건절 추가

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -12,203 +12,234 @@ import SafariServices
 import NetworkExtension
 import SystemConfiguration.CaptiveNetwork
 
+
+extension String {
+    func substring(from: Int, to: Int) -> String {
+        guard from < count, to >= 0, to - from >= 0 else {
+            return ""
+        }
+        
+        // Index 값 획득
+        let startIndex = index(self.startIndex, offsetBy: from)
+        let endIndex = index(self.startIndex, offsetBy: to + 1) // '+1'이 있는 이유: endIndex는 문자열의 마지막 그 다음을 가리키기 때문
+        
+        // 파싱
+        return String(self[startIndex ..< endIndex])
+    }
+}
+
 struct ContentView: View {
     
+    // 디바이스 ip 주소 반환 함수,
+    func getIPAddress() -> String {
+        var address: String?
+        var ifaddr: UnsafeMutablePointer<ifaddrs>? = nil
+        if getifaddrs(&ifaddr) == 0 {
+            var ptr = ifaddr
+            while ptr != nil {
+                defer { ptr = ptr?.pointee.ifa_next }
+
+                guard let interface = ptr?.pointee else { return "" }
+                let addrFamily = interface.ifa_addr.pointee.sa_family
+                if addrFamily == UInt8(AF_INET) || addrFamily == UInt8(AF_INET6) {
+
+                    let name: String = String(cString: (interface.ifa_name))
+                    if  name == "en0" || name == "en2" || name == "en3" || name == "en4" || name == "pdp_ip0" || name == "pdp_ip1" || name == "pdp_ip2" || name == "pdp_ip3" {
+                        var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+                        getnameinfo(interface.ifa_addr, socklen_t((interface.ifa_addr.pointee.sa_len)), &hostname, socklen_t(hostname.count), nil, socklen_t(0), NI_NUMERICHOST)
+                        address = String(cString: hostname)
+                    }
+                }
+            }
+            freeifaddrs(ifaddr)
+        }
+        return address ?? ""
+    }
+
+    // 유효한 auth.soongsil.ac.kr 로그인 주소를 생성하는 함수
+    func getURL() -> String {
+        
+        // IP주소를 파악
+        var ipAddress = getIPAddress()
+                
+        // vlangtag 선택
+        let vlangtagArr = ["0110", "0220", "0550"]
+        let vlangtagSelected = vlangtagArr.randomElement()!
+        
+        // 만약 모바일 데이터의 경우 & IPv6의 경우 가짜 아이피로 변경
+        if(ipAddress.count > 16){
+            ipAddress = "010.020.30.22"
+        }
+        
+        // uuid를 통해 가상 MAC주소 포멧을 생성
+        let uuid = NSUUID().uuidString
+        let macAddressTmp = uuid.substring(from: 24, to : 25) + ":"
+            + uuid.substring(from: 26, to : 27) + ":"
+            + uuid.substring(from: 28, to : 29) + ":"
+            + uuid.substring(from: 30, to : 31) + ":"
+            + uuid.substring(from: 32, to : 33) + ":"
+            + uuid.substring(from: 34, to : 35)
+        let macAddress = macAddressTmp
+        // let _ = print(macAddress)
+        
+        // 위 네트워크 정보를 통해 숭파이 로그인 페이지를 생성
+        return "http://auth.soongsil.ac.kr/login/login.do?ipaddress=" + ipAddress + "&macaddress=" + macAddress + "&vlantag=" + vlangtagSelected + "&sysid=0001&btype=014&scode=&back_url=192.168.0.1/login/login.cgi"
+        
+    }
+
+    
+    @State private var showSafariSoongfiLogin = false
+    @State private var showSafariRouterLogin = false
+    @State private var showHelp = false
+    @State private var connection = false
+    
     @Environment(\.openURL) private var openURL
     
     var body: some View {
-        NavigationView {
-                
-          Form {
-                            VStack {
-                                Text("숭파이는 공용 네트워크 문제 해결 도구입니다")
-                            }
-                            List {
-                                Section(header: Text("상황별 조치")){
-                                    
-                                    NavigationLink {
-                                        InternetNotConnectHelpBefore()
-                                    } label: {
-                                        Label("공용 와이파이 인터넷이 안돼요", systemImage: "wifi.exclamationmark")
-                                    }.isDetailLink(false)
-                        
-                                }
-                                
-                                Section(header: Text("피드백")) {
-                                    
-                                    Link(destination: URL(string:  "https://forms.gle/CiwkYGa2fhu4zuZFA")!) {
-                                        Label("버그 신고 및 건의", systemImage: "envelope")
-                                    }
-                                    
-                                }
-                                
-                                Section(header: Text("더보기")) {
-                                    
-                                    Link(destination: URL(string:  "https://hanarotg.github.io/others/soongfi/")!) {
-                                        Label("홈페이지", systemImage: "safari")
-                                    }
-                                    
-                                    Link(destination: URL(string:  "https://hanarotg.github.io/others/soongfi/private-policy.html")!) {
-                                        Label("개인정보 처리방침", systemImage: "person.fill")
-                                    } 
-                                                                        
-                                }
-                                }.navigationTitle("숭파이")
-
-                            }
-                    
-                }.navigationViewStyle(StackNavigationViewStyle())
-    }
-}
-
-struct InternetNotConnectHelpBefore : View {
-    
-    var body: some View {
+        VStack {
+            
             VStack {
-                ScrollView {
-                    
-                    VStack {
-                        
-                        VStack(alignment: .leading) {
-                            
-                            Text("공용 와이파이에 연결되어 있습니다.")
-                            
-                            Image("WifiConnected").resizable().scaledToFit()
-                                .cornerRadius(10)
-                                .shadow(radius: 1)
-                            
-                            Spacer().frame(height: 50)
-                            
-                            Text("공용 와이파이에 연결되어 있지만\n인터넷 사용이 불가능합니다.")
-                            
-                            Image("InternetNotConnected").resizable().scaledToFit()
-                                .cornerRadius(10)
-                                .shadow(radius: 1)
-                            
+             
+                Spacer()
+                
+                // auth.soongsil.ac.kr 로그인 서버로 직접 접속을 시도합니다.
+                // ip 및 mac vtag 등의 접속 요건 파라미터를 랜덤으로 생성합니다.
+                Button(action: {
+                    showSafariSoongfiLogin = true
+                })
+                {
+                    VStack(spacing: 0){
+                        Image("SoongfiLarge")
+                            .resizable()
+                            .frame(width: 192, height: 192)
+                        VStack {
+                            Text("숭파이 로그인")
+                                .foregroundColor(.white)
+                                .fontWeight(.bold)
+                                .padding()
                         }
-                    }.padding()
+                        .frame(width: 192)
+                        .background(Color.accentColor)
+                        
+                    }
+                }
+                .sheet(isPresented: $showSafariSoongfiLogin) {
+                    let URLtmp = getURL()
+                    SafariView(url:URL(string : URLtmp)!)
+                }
+                .cornerRadius(16)
+                .shadow(radius: 4, y: 2)
+                
+                Spacer()
+                
+                // 인터넷이 연결되어 있는 경우
+                if connection == true {
+                    Text("인터넷에 연결되어 있는 것 같아요!").foregroundColor(.white)
                 }
                 
-                NavigationLink(destination: InternetNotConnectedHelpList()) {
-                        Text("동일한 문제를 겪고 있습니다.")
-                            .padding()
-                            .frame(maxWidth: .infinity)
-                            .foregroundColor(Color.white)
-                            .background(Color.accentColor)
-                            .cornerRadius(8)
-                    
-                }.isDetailLink(false).navigationTitle("문제 인식").padding()
-                
-                
             }
-    }
-}
-
-
-struct InternetNotConnectedHelpList: View {
-    
-    @Environment(\.openURL) private var openURL
-    
-    var body: some View {
-        
-        ScrollView {
+            .frame(maxHeight: .infinity)
+                        
             VStack {
+                
+                // 라우터 아이피로 접속을 시도합니다.
+                Button(action: {
+                    showSafariRouterLogin = true
+                })
+                {
+                    Text("로그인 페이지 호출 실패")
+                        .padding()
+                        .frame(maxWidth: 536)
+                        .foregroundColor(Color.accentColor)
+                        .background(Color(.systemGray5))
+                        .cornerRadius(8)
+                }
+                .sheet(isPresented: $showSafariRouterLogin) {
+                    SafariView(url:URL(string: "http://192.168.0.1")!)
+                }
                 
                 Button(action: {
-                                       if let url = URL(string: "http://192.168.0.1"){
-                                           openURL(url)
-                                       }
-                                   })
-                                   {
-                                       Text("조치#1 공용 와이파이 라우터 접속")
-                                           .padding()
-                                           .frame(maxWidth: .infinity)
-                                           .foregroundColor(Color.white)
-                                           .background(Color.accentColor)
-                                           .cornerRadius(8)
-                                   }
-                                   
-                                   Button(action: {
-                                       if let url = URL(string: "http://www.gstatic.com/generate_204"){
-                                           openURL(url)
-                                       }
-                                   })
-                                   {
-                                       Text("조치#2 gstatic")
-                                           .padding()
-                                           .frame(maxWidth: .infinity)
-                                           .foregroundColor(Color.white)
-                                           .background(Color.accentColor)
-                                           .cornerRadius(8)
-                                   }
-                                   
-                                   Button(action: {
-                                       if let url = URL(string: "http://captive.apple.com/hotspot-detect.html"){
-                                           openURL(url)
-                                       }
-                                   })
-                                   {
-                                       Text("조치#3 apple captive portal")
-                                           .padding()
-                                           .frame(maxWidth: .infinity)
-                                           .foregroundColor(Color.white)
-                                           .background(Color.accentColor)
-                                           .cornerRadius(8)
-                                   }
-                                   
-               
+                    showHelp = true
+                })
+                {
+                    Text("도움말")
+                        .padding()
+                        .frame(maxWidth: 536)
+                        .foregroundColor(Color.accentColor)
+                        .background(Color(.systemGray5))
+                        .cornerRadius(8)
+                }
+                .sheet(isPresented: $showHelp) {
+                    ConnectionHelpView(showHelpView: $showHelp)
+                }
                 
-                               }
-                               
-                               Spacer().frame(minHeight: 30)
-          
-            Button(action: {
-                if let url = URL(string: "https://developer.apple.com/news/?id=q78sq5rv"){
-                    openURL(url)
-                }
-            })
-            {
-                Text("이 문제가 발생하는 이유(영문)")
-                    .padding()
-                    .frame(maxWidth: .infinity)
-                    .foregroundColor(Color.accentColor)
-                    .background(Color(.systemGray6))
-                    .cornerRadius(8)
-            }
+            }.frame(maxHeight: .infinity, alignment: .bottom)
             
-            
-            Button(action: {
-                if let url = URL(string: "https://forms.gle/CiwkYGa2fhu4zuZFA"){
-                    openURL(url)
-                }
-            })
-            {
-                Text("버그 신고 및 건의")
-                    .padding()
-                    .frame(maxWidth: .infinity)
-                    .foregroundColor(Color.accentColor)
-                    .background(Color(.systemGray6))
-                    .cornerRadius(8)
-            }
-            
-            }.padding().navigationTitle("조치")
-            
-        
+        }.padding()
+        .frame(maxWidth: .infinity)
+        .background(connection ? Color.accentColor : Color(.systemGray6))
     }
 }
 
-struct AppInfoView: View {
+
+struct SafariView: UIViewControllerRepresentable {
+    
+    let url : URL
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController {
+        return SFSafariViewController(url: url)
+    }
+
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) {
+
+    }
+
+}
+
+
+
+
+struct ConnectionHelpView: View {
+    
+    
+    @Binding var showHelpView: Bool
     
     @Environment(\.openURL) private var openURL
-
     
     var body: some View {
+        VStack(alignment: .leading) {
+            
+            
+                Button(action: { self.showHelpView.toggle() }){
+                    Image(systemName: "xmark.circle.fill")
+                        .imageScale(.large)
+                        .foregroundColor(Color(.systemGray3))
+                }.frame(maxWidth: .infinity, alignment: .trailing)
+        
+           
+            Text("도움말").font(.title)
+            Text("아래 가이드에 따라 가능한 여러 가지 방법을 시도해 보세요.").font(.subheadline).foregroundColor(Color.gray)
+            
+        }.padding()
         
         ScrollView {
-            VStack {
+            VStack(alignment: .leading) {
+                
+                
+                VStack(alignment: .leading) {
+                    Text("인터넷 속도가 느려요, 자꾸 끊겨요").font(.title2)
+                    
+                    VStack(alignment: .leading) {
+                        Text("1. Soongsil_WIFI 연결을 끊어주세요.")
+                        Text("2. 인터넷이 되는 다른 와이파이(또는 모바일 데이터)에 연결하세요.")
+                        Text("3. 숭파이 로그인 버튼을 눌러 로그인해주세요.")
+                        Text("4. 다시 Soongsil_WIFI에 연결하여 인터넷 속도를 확인해 주세요.")
+                    }.padding()
+    
+                }.padding()
                
                 VStack(alignment: .leading) {
-                    Text("건의 및 버그 신고").font(.title)
+                    Text("기타").font(.title2)
                     
                     Button(action: {
                         if let url = URL(string: "https://forms.gle/CiwkYGa2fhu4zuZFA"){
@@ -216,20 +247,13 @@ struct AppInfoView: View {
                         }
                     })
                     {
-                        Text("건의 및 버그 신고")
+                        Text("피드백(구글 폼)")
                             .padding()
                             .frame(maxWidth: .infinity)
                             .foregroundColor(Color.white)
                             .background(Color.accentColor)
                             .cornerRadius(8)
                     }
-                    
-                
-                    }.padding()
-            
-        
-                VStack(alignment: .leading) {
-                    Text("앱 정보").font(.title)
                     
                     Button(action: {
                         if let url = URL(string: "https://hanarotg.github.io/others/soongfi/"){
@@ -259,27 +283,14 @@ struct AppInfoView: View {
                             .cornerRadius(8)
                     }
                     
-                    Button(action: {
-                        if let url = URL(string: "https://github.com/hanarotg"){
-                            openURL(url)
-                        }
-                    })
-                    {
-                        Text("개발자 깃허브")
-                            .padding()
-                            .frame(maxWidth: .infinity)
-                            .foregroundColor(Color.accentColor)
-                            .background(Color(.systemGray6))
-                            .cornerRadius(8)
-                    }
-                    
                 }.padding()
-            
+                
+                
             }
+                
         }
     }
 }
-                        
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
